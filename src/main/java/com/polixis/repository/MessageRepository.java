2fe773ab6fda024bb928db30322260bac7bc0c0c
VazgenLevonyan <vazgenlevonyan1898@gmail.com>
2025-03-11 02:37:09 +0400

    Created repositories for project entites

---
+++ src/main/java/com/polixis/repository/MessageRepository.java
@@ -0,0 +1,9 @@
+package com.polixis.repository;
+
+import com.polixis.entity.Message;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class MessageRepository implements PanacheRepository<Message> {
+}
---
+++ src/main/java/com/polixis/repository/UsersGroupRepository.java
@@ -0,0 +1,12 @@
+package com.polixis.repository;
+
+import com.polixis.entity.UsersGroup;
+import io.quarkus.hibernate.orm.panache.PanacheRepository;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UsersGroupRepository implements PanacheRepository<UsersGroup> {
+    public  UsersGroup findByName(String name) {
+        return find("name", name).firstResult();
+    }
+}
